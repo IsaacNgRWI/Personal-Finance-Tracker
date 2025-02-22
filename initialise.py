f9c0e2ec4722d252b658673f7d7323d4bf346f02
Isaac <isaacngrwi@gmail.com>
2025-02-22 16:09:25 +0000

    Added method to establish and test connection with MongoDB server

--- initialise.py
+++ initialise.py
@@ -1,10 +1,22 @@
 import pymongo
 from pymongo.server_api import ServerApi
-import password as pw
+import passwords as pw
 
-class StartDb:
+class Start:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def start(self):
+    def startdb(self):
+        uri = f"mongodb+srv://{self.username}:{self.password}@cluster0.2o89z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        # creates a new client and connects it to the server
+        client =pymongo.MongoClient(uri, server_api=ServerApi("1"))
+
+        # sends a ping to test connection, returns a message if connection established or the error if not
+        try:
+            client.admin.command("ping")
+            print("MongoDB database successfully connected")
+            print("----------------------------------------------")
+        except Exception as e:
+            print(e)
+
