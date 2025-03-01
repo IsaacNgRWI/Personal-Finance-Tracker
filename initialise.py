245a8b949e631a4e9513abda9b7530551be466c3
Isaac <isaacngrwi@gmail.com>
2025-03-01 18:49:08 +0000

    Added method to return all or a specified number of records from a collection order by date descending

--- initialise.py
+++ initialise.py
@@ -7,6 +7,8 @@ class User:
     def __init__(self, username=pw.username, password=pw.password):
         self.username = username
         self.password = password
+        self.database1 = None
+        self.collection1 = None
 
     def startdb(self, database_name):
         uri = f"mongodb+srv://{self.username}:{self.password}@cluster0.2o89z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -33,5 +35,23 @@ class User:
         try:
             self.collection1.insert_one({"Item" : str(item_name), "Price" : float(item_price), "Category" : str(item_cat), "Date" : datetime.now()})
             print(f"{item_name} successfully added at {datetime.now()}")
+        except Exception as e:
+            print(f"An error has occurred: {e}")
+
+    def return_all(self, amount=None):
+        counter = 0
+        try:
+            if amount > 1:
+                print("The number of records requested cannot be less than 1")
+            elif amount is None:
+                for i in self.collection1.find.sort("Date", -1):
+                    counter += 1
+                    print(f"Returning all purchases:")
+                    print(f"{counter}. {i}")
+            else:
+                for i in self.collection1.find.sort("Date", -1).limit(int(amount)):
+                    counter += 1
+                    print(f"Returning {amount} most recent purchases:")
+                    print(f"{counter}. {i}")
         except Exception as e:
             print(f"An error has occurred: {e}")
