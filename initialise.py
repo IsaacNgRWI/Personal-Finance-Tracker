299b4abaf29591fd5791e5fa6986f106c8942b8c
Isaac <isaacngrwi@gmail.com>
2025-03-03 17:45:49 +0000

    Added method to query by item name and sort and limit results Also updated add_record_one method to allow for input of quantity of items purchased

--- initialise.py
+++ initialise.py
@@ -31,9 +31,9 @@ class User:
         except Exception as e:
             print(f"An error has occurred: {e}")
 
-    def add_record_one(self, item_name, item_price, item_cat):
+    def add_record_one(self, item_name, item_price, item_quantity, item_cat):
         try:
-            self.collection1.insert_one({"Item" : str(item_name), "Price" : float(item_price), "Category" : str(item_cat), "Date" : datetime.now()})
+            self.collection1.insert_one({"item_name" : str(item_name), "item_price" : float(item_price),"item_quantity" : int(item_quantity),  "item_cat" : str(item_cat), "purchase_date" : datetime.now()})
             print(f"{item_name} successfully added at {datetime.now()}")
         except Exception as e:
             print(f"An error has occurred: {e}")
@@ -52,6 +52,24 @@ class User:
                 for i in self.collection1.find.sort("Date", -1).limit(int(amount)):
                     counter += 1
                     print(f"Returning {amount} most recent purchases:")
+                    print(f"{counter}. {i}")
+        except Exception as e:
+            print(f"An error has occurred: {e}")
+
+    def query_by_item_name(self, name, amount=None, sort=-1):
+        counter = 0
+        try:
+            if amount > 1:
+                print("The number of records requested cannot be less than 1")
+            elif amount is None:
+                for i in self.collection1.find({"item_name": name}).sort(sort):
+                    counter += 1
+                    print("Returning all purchases that match description")
+                    print(f"{counter}. {i}")
+            else:
+                for i in self.collection1.find({"item_name" : name}).sort(sort).limit(amount):
+                    counter += 1
+                    print(f"Returning {amount} purchases matching that description")
                     print(f"{counter}. {i}")
         except Exception as e:
             print(f"An error has occurred: {e}")
